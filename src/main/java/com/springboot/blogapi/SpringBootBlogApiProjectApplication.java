2878d8affffa41e0ff6b17791ec69383838022f5
Vishal Singh <vishalsingh.vh99@gmail.com>
2023-08-22 13:11:28 +0530

    First commit

---
+++ src/main/java/com/springboot/blogapi/SpringBootBlogApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.blogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/blogapi/SpringBootBlogApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
